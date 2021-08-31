1c756e1c64a60468b4fa4b9a7c5594b36c975382
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-08-31 15:57:44 -0400

    getters placed

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
 // Your code goes here
-//i agree
+
+//getters//
+const body = document.querySelector('body');
+
+const mainNav = document.querySelector('.main-navigation');
+const navContainer = mainNav.document.querySelector('.nav-container');
+const logoh1 = navContainer.querySelector('.logo-heading');
+const nav = navContainer.querySelector('nav');
+
+const containerHome = document.querySelector('.home');
+const intro = containerHome.querySelector('.intro');
+const introimg = intro.querySelector('img');
+const introh2 = intro.querySelector('h2');
+const introtext = intro.querySelector('p');
+//content section getters
+const contentSection = containerHome.querySelector('.content-secttion');
+const copntentText = contentSection.querySelector('.text-content');//has h2 and 2 p
+const imgContent = containerHome.querySelector('.img-content img');//direct to img
+
+//inverse getters
+const inverseContent = containerHome.querySelector('.inverse-content');
+const inverseimg = inverseContent.querySelector('.img-content img');
+const inversetext = inverseContent.querySelector('.text-content');
